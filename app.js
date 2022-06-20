13c2936b61d58ff279c710e6276bce588fe56c57
Kağan Çubukçu <kagancancubukcu@gmail.com>
2022-06-20 17:17:43 +0300

    update files

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+
+const ejs = require("ejs");
+const Post = require("./models/Post");
+const pageControllers = require("./controllers/pageControllers");
+const postControllers = require("./controllers/postControllers");
+const app = express();
+
+//Connect
+const CONNECTION_URL =
+  "mongodb+srv://cleanblog:cleanblog@cluster0.j4pud.mongodb.net/cleanblog-db?retryWrites=true&w=majority";
+mongoose.connect(
+  CONNECTION_URL,
+  // {
+  //   useNewUrlParser: true,
+
+  //   useUnifiedTopology: true,
+  // },
+  (err) => {
+    if (err) throw err;
+    console.log("Connected to MongoDB!!!");
+  }
+);
+
+app.set("view engine", "ejs");
+
+//Middlewares
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method", { methods: ["POST", "GET"] }));
+
+//Routes
+app.get("/", postControllers.getAllPost);
+app.get("/posts/:id", postControllers.getPost);
+app.post("/posts", postControllers.createPost);
+app.put("/posts/:id", postControllers.updatePost);
+app.delete("/posts/:id", postControllers.deletePost);
+
+app.get("/about", pageControllers.getAboutPage);
+app.get("/add", pageControllers.getAddPage);
+
+app.get("/posts/edit/:id", pageControllers.getEditPage);
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı`);
+});
